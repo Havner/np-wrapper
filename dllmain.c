cf380a6d9026d834ec7f473d561d5b696b505bae
Lukasz Pawelczyk <havner@gmail.com>
2024-08-15 23:38:07 +0200

    Don't initialize library on every thread

--- dllmain.c
+++ dllmain.c
@@ -244,8 +244,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
         return initialize();
+    case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
